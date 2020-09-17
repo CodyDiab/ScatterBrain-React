a510a999d2208f56e8b2652d622d32f511d69ede
Diab <emailcodydiab@gmail.com>
2020-09-17 09:11:02 -0500

    backend/set user model

---
+++ Server/models/User.js
@@ -0,0 +1,55 @@
+const {Schema, model} = require('mongoose');
+const bcrypt = require('bcrypt');
+
+//inport subject schema
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+        
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must use a valid email address'],
+
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        subjects: [subjectSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        }
+    }
+);
+    // hash password
+    userSchema.pre('save', async function (next) {
+        if (this.isNew || this.isModified('password')) {
+          const saltRounds = 10;
+          this.password = await bcrypt.hash(this.password, saltRounds);
+        }
+      next();
+    });
+    
+    // custom method to compare and validate password for logging in
+    userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+    };
+   
+    
+   // when we query a user, we'll also get another field called `subjectCount` with the number of saved subjects we have
+    userSchema.virtual('subjectCount').get(function () {
+    return this.subjects.length;
+    });
+
+const User = model('User', userSchema);
+
+module.exports = User;
