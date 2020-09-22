ae6c3aa2b41692a61ad771c6268bb61d394faf35
Diab <emailcodydiab@gmail.com>
2020-09-22 09:12:36 -0500

    set up directory to use apollo/graphql

--- Server/server.js
+++ Server/server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-// const { ApolloServer } = require('apollo-server-express');
-// const { typeDefs, resolvers } = require('./schemas');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 const routes = require('./routes');
-// const { authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   context: authMiddleware
-// });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
-// server.applyMiddleware({ app });
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
