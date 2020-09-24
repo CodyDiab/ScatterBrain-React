2274e5f827f549c80dd3639292d03f2eddc3c311
Diab <emailcodydiab@gmail.com>
2020-09-24 10:19:15 -0500

    comment layout of required resolvers

---
+++ Server/schemas/resolvers.js
@@ -0,0 +1,14 @@
+
+
+
+// create user
+// login user
+//log out user
+//create subject
+//get seubjects
+//get single subject with resources 
+//edit subject
+//delete subject
+//add resource
+//edit resource
+//delete resource 
