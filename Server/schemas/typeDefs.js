b6537a163705b291ec7874d2b1dbaa48128d3f4f
Diab <emailcodydiab@gmail.com>
2020-09-25 07:55:26 -0500

    define type defs

--- Server/schemas/typeDefs.js
+++ Server/schemas/typeDefs.js
@@ -10,9 +10,36 @@ type User {
 type Subject {
     _id: ID
     title: String
-    resources: [resources]
+    createdAt: String
+    resources: [Resource]
 
 }
+type Resource {
+    _id : ID
+    title: String
+    url: String
+    notes: String
+    createdAt: String
 
+
+}
+type Auth {
+    token: ID!
+    user: User
+}
+type Query {
+    me: User
+    user(username: String!): User
+    subjects(username: String!):[Subject]
+    subject(_id: ID!): Subject
+}
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addSubject(title: String!):Subject
+    addResource(title: String!,url: String!,notes: String!)Subject
+    deleteSubject(_id: ID!):Subject
+    deleteResource(_id: ID!):Subject
+}
 
 `
